dd24de369b6dfc33a718ef5f30cf6e296e90ba5f
Code-Break0 <41364583+Code-Break0@users.noreply.github.com>
2020-12-29 14:02:01 -0800

    Update utils.php

--- php/utils.php
+++ php/utils.php
@@ -39,3 +39,30 @@
 		$stmt->close();
 		return -1;
 	}
+
+
+
+	function createToken() {
+		$seed = random_bytes(8);
+		$t = time();
+		$hash = hash_hmac('sha256', session_id() . $seed . $t, CSRF_TOKEN_SECRET, true);
+		return urlSafeEncode($hash . '|' . $seed . '|' . $t);
+	}
+
+	function validateToken($token) {
+		$parts = explode('|', urlSafeDecode($token));
+		if(count($parts) === 3) {
+			$hash = hash_hmac('sha256', session_id() . $parts[1] . $parts[2], CSRF_TOKEN_SECRET, true);
+			if(hash_equals($hash, $parts[0])) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	function urlSafeEncode($m) {
+		return rtrim(strtr(base64_encode($m), '+/', '-_'), '=');
+	}
+	function urlSafeDecode($m) {
+		return base64_decode(strtr($m, '-_', '+/'));
+	}
