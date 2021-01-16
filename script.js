c277a6aaa8422c7cf383c5bcf1a2fdab9976c39e
Code-Break0 <41364583+Code-Break0@users.noreply.github.com>
2021-01-16 10:28:22 -0800

    Update script.js

--- script.js
+++ script.js
@@ -28,11 +28,45 @@ function request(url, data, callback) {
 
 
 // index.php
-function logout() {}
+function logout() {
+	request('php/logout.php', false, function(data) {
+		window.location = 'login';
+	});
+}
 function deleteAccount() {}
 
 // login.php
-function login() {}
+function login() {
+	request('php/login.php', '#loginForm', function(data) {
+		document.getElementById('errs').innerHTML = "";
+		var transition = document.getElementById('errs').style.transition;
+		document.getElementById('errs').style.transition = "none";
+		document.getElementById('errs').style.opacity = 0;
+		switch(data) {
+			case '0':
+				window.location = './';
+				break;
+			case '1':
+				document.getElementById('errs').innerHTML += '<div class="err">Incorrect username or password</div>';
+				break;
+			case '2':
+				document.getElementById('errs').innerHTML += '<div class="err">Failed to connect to database. Please try again later.</div>';
+				break;
+			case '3':
+				document.getElementById('errs').innerHTML += '<div class="err">You have excedded the max number of login attempts per hour. Try again in an hour.</div>';
+				break;
+			case 4:
+				document.getElementById('errs').innerHTML += '<div class="err">Your email has not been validated. Please check your email for a validation link or <a href="./validate">click here</a> to send another link</div>';
+				break;
+			default:
+				document.getElementById('errs').innerHTML += '<div class="err">An unknown error occured. Please try again later.</div>';
+		}
+		setTimeout(function() {
+			document.getElementById('errs').style.transition = transition;
+			document.getElementById('errs').style.opacity = 1;
+		}, 10);
+	});
+}
 
 // register.php
 function register() {
