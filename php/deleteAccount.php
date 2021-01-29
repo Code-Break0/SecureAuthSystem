35b98aaeff6661b7db5dbf671e064b2694615fee
Code-Break0 <41364583+Code-Break0@users.noreply.github.com>
2021-01-28 16:33:06 -0800

    Create deleteAccount.php

---
+++ php/deleteAccount.php
@@ -0,0 +1,32 @@
+<?php
+	require_once 'utils.php';
+	session_start();
+
+
+	if(isset($_POST['csrf_token']) && validateToken($_POST['csrf_token'])) {
+		if(isset($_SESSION['loggedin']) && isset($_SESSION['userID']) && $_SESSION['loggedin'] === true) {
+			$C = connect();
+			if($C) {
+				if(sqlUpdate($C, 'DELETE FROM users WHERE id=?', 'i', $_SESSION['userID'])) {
+					sqlUpdate($C, 'DELETE FROM requests WHERE user=?', 'i', $_SESSION['userID']);
+					sqlUpdate($C, 'DELETE FROM loginattempts WHERE user=?', 'i', $_SESSION['userID']);
+					session_destroy();
+					echo 0;
+				}
+				else {
+					echo 1;
+				}
+				$C->close();
+			}
+			else {
+				echo 2;
+			}
+		}
+		else {
+			echo 3;
+		}
+	}
+	else {
+		echo 4;
+	}
+
