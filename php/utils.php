b589e6ac4de8ba6fbfbdbac6238cf2def551db37
Code-Break0 <41364583+Code-Break0@users.noreply.github.com>
2021-01-31 18:04:19 -0800

    Update utils.php

--- php/utils.php
+++ php/utils.php
@@ -64,7 +64,7 @@
 	function createToken() {
 		$seed = urlSafeEncode(random_bytes(8));
 		$t = time();
-		$hash = hash_hmac('sha256', session_id() . $seed . $t, CSRF_TOKEN_SECRET, true);
+		$hash = urlSafeEncode(hash_hmac('sha256', session_id() . $seed . $t, CSRF_TOKEN_SECRET, true));
 		return urlSafeEncode($hash . '|' . $seed . '|' . $t);
 	}
 
@@ -72,7 +72,7 @@
 		$parts = explode('|', urlSafeDecode($token));
 		if(count($parts) === 3) {
 			$hash = hash_hmac('sha256', session_id() . $parts[1] . $parts[2], CSRF_TOKEN_SECRET, true);
-			if(hash_equals($hash, $parts[0])) {
+			if(hash_equals($hash, urlSafeDecode($parts[0]))) {
 				return true;
 			}
 		}
