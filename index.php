3dbc333d395c6069fa1f89af793aedd5e9ad0f89
Code-Break0 <41364583+Code-Break0@users.noreply.github.com>
2021-05-06 14:49:04 -0700

    XSS protection

--- index.php
+++ index.php
@@ -2,8 +2,24 @@
 	require_once 'php/utils.php'; 
 	session_start();
 
+	
 	if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
 		header("Location: login");
+		exit;
+	}
+
+	$user = [];	
+	$C = connect();
+	if($C) {
+		$res = sqlSelect($C, 'SELECT * FROM users WHERE id=?', 'i', $_SESSION['userID']);
+		if($res && $res->num_rows === 1) {
+			$user = $res->fetch_assoc();
+		}
+		else {
+			exit;
+		}
+	}
+	else {
 		exit;
 	}
 
@@ -23,6 +39,8 @@
 		<h1>Secure Site</h1>
 		<div id="errs" class="errorcontainer"></div>
 		<br><br>
+		<h2>Hello <?php echo htmlspecialchars($user['name'], ENT_QUOTES); ?></h2>
+		<br><br>
 		<div class="btn" onclick="logout();">Log Out</div>
 		<br><br>
 		<div class="btn" onclick="deleteAccount();">Delete Account</div>
@@ -30,7 +48,11 @@
 	
 	<svg class="wave" xmlns="http://www.w3.org/2000/svg" viewBox="0 32 1440 320"><defs><linearGradient id="a" x1="50%" x2="50%" y1="-10.959%" y2="100%"><stop stop-color="#ffffff" stop-opacity=".10" offset="0%"/><stop stop-color="#FFFFFF" stop-opacity=".05" offset="100%"/></linearGradient></defs><path fill="url(#a)" fill-opacity="1" d="M 0 320 L 48 288 C 96 256 192 192 288 160 C 384 128 480 128 576 112 C 672 96 768 64 864 48 C 960 32 1056 32 1152 32 C 1248 32 1344 32 1392 32 L 1440 32 L 1440 2000 L 1392 2000 C 1344 2000 1248 2000 1152 2000 C 1056 2000 960 2000 864 2000 C 768 2000 672 2000 576 2000 C 480 2000 384 2000 288 2000 C 192 2000 96 2000 48 2000 L 0 2000 Z"></path></svg>
 	<svg class="wave" xmlns="http://www.w3.org/2000/svg" viewBox="0 32 1440 320"><defs><linearGradient id="a" x1="50%" x2="50%" y1="-10.959%" y2="100%"><stop stop-color="#ffffff" stop-opacity=".10" offset="0%"/><stop stop-color="#FFFFFF" stop-opacity=".05" offset="100%"/></linearGradient></defs><path fill="url(#a)" fill-opacity="1" d="M 0 320 L 48 288 C 96 256 192 192 288 160 C 384 128 480 128 576 112 C 672 96 768 64 864 48 C 960 32 1056 32 1152 32 C 1248 32 1344 32 1392 32 L 1440 32 L 1440 2000 L 1392 2000 C 1344 2000 1248 2000 1152 2000 C 1056 2000 960 2000 864 2000 C 768 2000 672 2000 576 2000 C 480 2000 384 2000 288 2000 C 192 2000 96 2000 48 2000 L 0 2000 Z"></path></svg>
-
 	<script src="script.js"></script>
+	<div class="hidden" id="data"><?php echo htmlspecialchars(json_encode($user), ENT_QUOTES); ?></div>
+	<script>
+		var user = JSON.parse(document.getElementById('data').textContent);
+	</script>
+
 </body>
 </html>
