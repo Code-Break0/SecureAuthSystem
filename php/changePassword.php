940cdea493ae3a0420e94c36f62ae8bf62a6811e
Code-Break0 <41364583+Code-Break0@users.noreply.github.com>
2021-05-09 14:59:35 -0700

    Add files via upload

---
+++ php/changePassword.php
@@ -0,0 +1,87 @@
+<?php
+	require_once 'utils.php';
+	session_start();
+
+
+	// Validate post variables & CSRF token
+	$errors = [];
+
+	if(empty($_POST['id'])) {
+		// $errors[] = 'No ID';
+		$errors[] = 1;
+	}
+	if(empty($_POST['hash'])) {
+		// $errors[] = 'No hash';
+		$errors[] = 2;
+	}
+	if(!isset($_POST['password']) || !preg_match('/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[\~?!@#\$%\^&\*])(?=.{8,})/', $_POST['password'])) {
+		// $errors[] = 'Password must have upper & lower letters + at least one number + at least one symbol and be 8 or more chars long';
+		$errors[] = 3;
+	}
+	else if(!isset($_POST['confirm-password']) || $_POST['confirm-password'] !== $_POST['password']) {
+		// $errors[] = 'Passwords do not match';
+		$errors[] = 4;
+	}
+
+	if(count($errors) === 0) {
+		if(isset($_POST['csrf_token']) && validateToken($_POST['csrf_token'])) {
+			// Connect to Database
+			$C = connect();
+			if($C) {
+				// Get user, hash, & timestamp from requests
+				$res = sqlSelect($C, 'SELECT user,hash,timestamp FROM requests WHERE id=? LIMIT 1', 'i', $_POST['id']);
+				if($res && $res->num_rows === 1) {
+					$request = $res->fetch_assoc();
+
+					// password_verify hash
+					if(password_verify(urlSafeDecode($_POST['hash']), $request['hash'])) {
+						// check if request is expired
+						if($request['timestamp'] >= time() - PASSWORD_RESET_REQUEST_EXPIRY_TIME) {
+							// update password
+							$hash = password_hash($_POST['password'], PASSWORD_DEFAULT);
+							if(sqlUpdate($C, 'UPDATE users SET password=? WHERE id=?', 'si', $hash, $request['user'])) {
+								// delete all password reset requests for user
+								sqlUpdate($C, 'DELETE FROM requests WHERE user=? AND type=1', 'i', $request['user']);
+								$errors[] = 0;
+							}
+							else {
+								// $errors[] = 'Failed to update password';
+								$errors[] = 5;
+							}
+							
+						}
+						else {
+							// $errors[] = 'This reset request has expired';
+							$errors[] = 6;
+						}
+					}
+					else {
+						// $errors[] = 'Invalid password reset request';
+						$errors[] = 7;
+					}
+
+					$res->free_result();
+				}
+				else {
+					// $errors[] = 'Invalid password reset request';
+					$errors[] = 7;
+				}
+				$C->close();
+			}
+			else {
+				// $errors[] = 'Failed to connect to database';
+				$errors[] = 8;
+			}
+		}
+		else {
+			// $errors[] = 'Invalid CSRF token';
+			$errors[] = 9;
+		}
+	}
+
+	echo json_encode($errors);
+	
+	
+	
+	
+
---
+++ php/passwordResetRequest.php
@@ -0,0 +1,71 @@
+<?php
+	require_once 'utils.php';
+	session_start();
+
+	// Validate post variables & CSRF token
+	if(!empty($_POST['email'])) {
+		if(!empty($_POST['csrf_token']) && validateToken($_POST['csrf_token'])) {
+			// Connect to database
+			$C = connect();
+			if($C) {
+				// Select id & name from users database and count requests
+				$dayago = time() - 60 * 60 * 24;
+				$res = sqlSelect($C, 'SELECT users.id,name,COUNT(requests.id) FROM users LEFT JOIN requests ON users.id = user AND type=1 AND timestamp>? WHERE email=? GROUP BY users.id', 'is', $dayago, $_POST['email']);
+				if($res && $res->num_rows === 1) {
+					$user = $res->fetch_assoc();
+
+					// Check if # of requests per day is valid
+					if($user['COUNT(requests.id)'] < MAX_PASSWORD_RESET_REQUESTS_PER_DAY) {
+						// create code & insert into requests database
+						$code = random_bytes(16); # 128 bit code 
+						$hash = password_hash($code, PASSWORD_DEFAULT);
+						$insertID = sqlInsert($C, 'INSERT INTO requests VALUES (NULL, ?, ?, ?, 1)', 'isi', $user['id'], $hash, time());
+						if($insertID !== -1) {
+							// Send email with link to user
+							$msg = '<a href="http://localhost/Youtube/SecureAccountSystem/resetpassword/' . $insertID . '/' . urlSafeEncode($code) .'">Click Here to Reset your Password</a>';
+							if(sendEmail($_POST['email'], $user['name'], 'Password Reset', $msg)) {
+								// echo 'An email has been sent if an account with that email exists';
+								echo 0;
+							}
+							else {
+								// echo 'Failed to send email';
+								echo 1;
+							}
+						}
+						else {
+							// echo 'Failed to create request in database';
+							echo 2;
+						}
+					}
+					else {
+						// echo 'Too many requests in the last 24 hours... try again later';
+						echo 3;
+					}
+
+					$res->free_result();
+				}
+				else {
+					// echo 'An email has been sent if an account with that email exists';
+					echo 0;
+				}
+				$C->close();
+			}
+			else {
+				// echo 'Failed to connect to database';
+				echo 4;
+			}
+		}
+		else {
+			// echo 'Invalid CSRF token';
+			echo 5;
+		}
+	}
+	else {
+		// echo 'You must enter an email';
+		echo 6;
+	}
+	
+	
+	
+	
+	
