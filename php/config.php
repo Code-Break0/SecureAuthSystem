2b1a7e6442dc00116f4c932b3a257a0a14d0b08d
Code-Break0 <41364583+Code-Break0@users.noreply.github.com>
2021-10-02 14:42:42 -0700

    Update config.php

--- php/config.php
+++ php/config.php
@@ -1,28 +1,29 @@
 <?php
 
 	// Database Credentials
-	define("DB_HOST", '<your database hostname>');
-	define("DB_DATABASE", '<your database name>');
-	define("DB_USERNAME", '<your database username>');
-	define("DB_PASSWORD", '<your database password>');
+	define('DB_HOST', '<your database hostname>');
+	define('DB_DATABASE', '<your database name>');
+	define('DB_USERNAME', '<your database username>');
+	define('DB_PASSWORD', '<your database password>');
 
 	// Email Credentials
-	define("SMTP_HOST", 'mail.mydomain.com');
-	define("SMTP_PORT", 465);
-	define("SMTP_USERNAME", 'name@mydomain.com');
-	define("SMTP_PASSWORD", '<my password>');
-	define("SMTP_FROM", 'noreply@mydomain.com');
-	define("SMTP_FROM_NAME", '<your name>');
+	define('SMTP_HOST', 'mail.mydomain.com');
+	define('SMTP_PORT', 465);
+	define('SMTP_USERNAME', 'name@mydomain.com');
+	define('SMTP_PASSWORD', '<my password>');
+	define('SMTP_FROM', 'noreply@mydomain.com');
+	define('SMTP_FROM_NAME', '<your name>');
 
 	// Global Variables
-	define("MAX_LOGIN_ATTEMPTS_PER_HOUR", 5);
-	define("MAX_EMAIL_VERIFICATION_REQUESTS_PER_DAY", 3);
-	define("MAX_PASSWORD_RESET_REQUESTS_PER_DAY", 3);
-	define("PASSWORD_RESET_REQUEST_EXPIRY_TIME", 60*60);
-	define("CSRF_TOKEN_SECRET", '<change me to something random>');
+	define('MAX_LOGIN_ATTEMPTS_PER_HOUR', 5);
+	define('MAX_EMAIL_VERIFICATION_REQUESTS_PER_DAY', 3);
+	define('MAX_PASSWORD_RESET_REQUESTS_PER_DAY', 3);
+	define('PASSWORD_RESET_REQUEST_EXPIRY_TIME', 60*60);
+	define('CSRF_TOKEN_SECRET', '<change me to something random>');
+	define('VALIDATE_EMAIL_ENDPOINT', 'http://localhost/YouTube/SecureAccountSystem/validate/');
 
 	// Code we want to run on every page/script
-	date_default_timezone_set("UTC"); 
-	// error_reporting(0);
+	date_default_timezone_set('UTC'); 
+	error_reporting(0);
 	session_set_cookie_params(['samesite' => 'Strict']);
 	
